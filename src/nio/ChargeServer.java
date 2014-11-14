05c2e40daadfc7ab992893c0b0702818ca942f0c
ShasowWakler <wangjie9012@126.com>
2014-11-14 20:49:10 +0800

    nio Server and Client

---
+++ src/nio/ChargeServer.java
@@ -0,0 +1,87 @@
+package nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ChargeServer {
+
+	private final static int PORT = 9090; 
+	
+	public static void main(String[] args) {
+		System.out.println("在端口"+PORT+"监听连接");
+		
+		byte[] content = new byte[100];
+		for(int i = 0 ; i < 100 ;i++){
+			content[i] = (byte)i;
+		}
+		
+		ServerSocketChannel serverChannel;
+		Selector sel = null;
+		try {
+			serverChannel = ServerSocketChannel.open();
+			ServerSocket serverSocket = serverChannel.socket();
+			SocketAddress endpoint = new InetSocketAddress(PORT);
+			serverSocket.bind(endpoint);
+			serverChannel.configureBlocking(false);
+			sel = Selector.open();
+			serverChannel.register(sel, SelectionKey.OP_ACCEPT);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while(true){
+			try {
+				sel.select();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Set<SelectionKey> iterator = sel.selectedKeys();
+			Iterator<SelectionKey> it = iterator.iterator();
+			while(it.hasNext()){
+				SelectionKey key = it.next();
+				if(key.isAcceptable()){
+					//服务器端
+					ServerSocketChannel server = (ServerSocketChannel) key.channel();
+					try {
+						SocketChannel client = server.accept();
+						System.out.println("接受客户端：" + client);
+						client.configureBlocking(false);
+						SelectionKey key2 = client.register(sel, SelectionKey.OP_WRITE);
+						ByteBuffer buffer = ByteBuffer.allocate(100);
+						buffer.put(content);
+						buffer.flip();
+						key2.attach(buffer);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				else if(key.isWritable()){
+					//客户端
+					SocketChannel client  = (SocketChannel) key.channel();
+					ByteBuffer buffer = (ByteBuffer) key.attachment();
+					if(!buffer.hasRemaining()){
+						buffer.flip();
+					}
+					try {
+						client.write(buffer);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/nio/ChargenClient.java
@@ -0,0 +1,30 @@
+package nio;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.WritableByteChannel;
+
+public class ChargenClient {
+
+	private final static byte[] ADDR = {(byte) 192,(byte) 168,1,102};
+	private final static int PORT = 9090;
+	private final static int SIZE = 100;
+	
+	public static void main(String[] args) throws IOException {
+		SocketAddress clientAddress = new InetSocketAddress( InetAddress.getByAddress(ADDR), PORT);
+		SocketChannel client = SocketChannel.open(clientAddress);
+		
+		ByteBuffer buffer = ByteBuffer.allocate(SIZE);
+		WritableByteChannel out = Channels.newChannel(System.out);
+		while(client.read(buffer) != -1){
+			buffer.flip();
+			out.write(buffer);
+			buffer.clear();
+		}
+	}
+}
